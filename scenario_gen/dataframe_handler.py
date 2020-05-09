0f73575bc5d6ff8cb117728b1462f02e52753f4d
KennethWebber <31325329+KennethWebber@users.noreply.github.com>
2020-05-08 19:09:06 -0500

    formatted to auto run

--- scenario_gen/dataframe_handler.py
+++ scenario_gen/dataframe_handler.py
@@ -1,5 +1,6 @@
-from test import *
+from test_engine import run_test
 import Greedy
+from scenario import Scenario
 import copy
 
 import pandas as pd
@@ -40,7 +41,7 @@ def evaluate_trial(df, trial_num):
     average_wait_time = df['Wait Time'].mean()
     return {trial_num : (average_wait_time)}
 
-def run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials = 10):
+def run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials = 10,print_trace=False):
     '''
     Runs trials.
     Returns pandas dataframe of wait time by trial number.
@@ -49,11 +50,11 @@ def run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials = 10):
     greedy_trials = {}
     #Run num_trials trials and store in dictionary
     for trial_num in range(num_trials):
-        greedy_scen = Scenario(sp1, sp2, sp3, sp4, sp5, sp6)
+        greedy_scen = Scenanrio(sp1, sp2, sp3, sp4, sp5, sp6)
         util_scen = copy.deepcopy(greedy_scen)
         # Pass a fresh scenario to each trial with the same properties
         #Utility test
-        (request_list, listOfPickupsAndDropoffs) = run_test(util_scen, print_trace=False)
+        (request_list, listOfPickupsAndDropoffs) = run_test(util_scen, print_trace=print_trace)
         df_util = generate_utility_dataframe(request_list, listOfPickupsAndDropoffs)
         utility_trials.update(evaluate_trial(df_util, trial_num))
         #Greedy test
@@ -67,7 +68,7 @@ def run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials = 10):
     df_greedy_trials.index.name = 'Trial #'
     return df_utility_trials, df_greedy_trials
 
-def evaluate_batch(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials=30):
+def evaluate_batch(sp1, sp2, sp3, sp4, sp5, sp6 , print_trace=False, num_trials=30):
     '''
     This function will run a batch of num_trials trials. It will print out the average wait times for each trial
     and the overall average wait time across all batches.
@@ -75,19 +76,58 @@ def evaluate_batch(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials=30):
         Pandas version 1.0.0 and later have a new to_markdown function. 
         I've included this check for automatic running in earlier packages.
     '''
-    df_utility_trials, df_greedy_trials = run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials)
+    df_utility_trials, df_greedy_trials = run_trials(sp1, sp2, sp3, sp4, sp5, sp6 , num_trials, print_trace=print_trace)
     print('Greedy Trials')
     print(df_greedy_trials.to_markdown())
     print('Utility Trials')
     print(df_utility_trials.to_markdown())
     print('\nTotal Average wait time across {0} trials :\nGreedy: {1:.6}\nUtility: {2:.6}'.format( num_trials, df_greedy_trials['Mean Wait Time'].mean(), df_utility_trials['Mean Wait Time'].mean() ))  
 
-#Scenario 1: Small Example
-sp1, sp2, sp3, sp4, sp5, sp6 = (10, 30, 5, 50, 10, 3)
-evaluate_batch(sp1, sp2, sp3, sp4, sp5, sp6)
+def evaluate_scenario_batch(scenario_list, label, print_trace=False):
+    '''
+    This function takes a list of tuples containing the parameters specifying
+    settings for a scenario
+    '''
+    for scenario_params in scenario_list:
+        sp1, sp2, sp3, sp4, sp5, sp6 = scenario_params
+        print(label)
+        evaluate_batch(sp1, sp2, sp3, sp4, sp5, sp6, print_trace=print_trace)
 
-#Scenario 2: Large Example
-#sp1, sp2, sp3, sp4, sp5, sp6 = (100, 300, 30, 200, 10, 10)
+def auto_run():
+    #number of nodes
+    evaluate_scenario_batch([(5,5,5,50,10,5)
+    ,(10,10,5,50,10,5)\
+   # ,(20,20,5,50,10,5)
+   # ,(40,40,5,50,10,5)
+    ,(80,80,5,50,10,5)]
+    , 'number of nodes', print_trace=True)
 
-#Scenario 3: Low Warehouse
-#sp1, sp2, sp3, sp4, sp5, sp6 = (2,30,10,100,10,2)
+    
+    #number of requests
+    evaluate_scenario_batch([(30,30,5,5,10,5)
+    ,(30,30,5,10,10,5)
+    ,(30,30,5,20,10,5)
+    ,(30,30,5,40,10,5)
+    ,(30,30,5,80,10,5)]
+    , 'number of requests')
+
+    #drone capacity
+    evaluate_scenario_batch([(30,30,5,100,10,5)
+    ,(30,30,5,100,10,10)
+    ,(30,30,5,100,10,20)
+    ,(30,30,5,100,10,40)
+    ,(30,30,5,100,10,80)]
+    , 'drone capacity')
+
+    #number of drones
+    evaluate_scenario_batch([(40,40,5,50,10,5)
+    ,(40,40,10,50,10,5)
+    ,(40,40,20,50,10,5)
+    ,(40,40,80,50,10,5)]
+    , 'number of drones')
+
+def demo():
+    evaluate_scenario_batch([(5,5,5,50,10,5)], 'demo')
+
+#auto_run()
+demo()
